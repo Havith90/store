10c6c1fdcdb45a29a849b0d71ad82039251c1ca5
Javi Segura <javiera.segurav@gmail.com>
2021-11-04 22:03:55 -0300

    namespaces, editando rutas, actualizando todos los modelos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password  
+
   belongs_to :role
+  has_many :orders
+  
+  validates :username, presence: true, uniqueness: true
+  validates :role_id, presence: true
+
+  #en gemfile ->línea 23: quitar gato a gem 'bcrypt', '~> 3.1.7'  #gema q habilita security password
 end
